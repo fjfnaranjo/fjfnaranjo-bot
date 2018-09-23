e64242d05b010fff83703d422264c79d1da1a5eb
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2018-09-23 23:20:59 +0200

    Improve new gunicorn+bot interactions

--- fjfnaranjobot/bot.py
+++ fjfnaranjobot/bot.py
@@ -1,30 +1,59 @@
-from telegram.ext import Updater, StringCommandHandler
+from json import loads, JSONDecodeError
+
+from telegram import Bot as TBot, Update
+from telegram.ext import Dispatcher
 
 from fjfnaranjobot import BOT_TOKEN, BOT_WEBHOOK_URL, BOT_WEBHOOK_TOKEN
 
 
+class BotLibraryError(Exception):
+    pass
+
+
+class BotJSONError(Exception):
+    pass
+
+
+class BotTokenError(Exception):
+    pass
+
+
 class Bot:
     def __init__(self):
-        self.updater = Updater(token=BOT_TOKEN)
-        self.dispatcher = self.updater.dispatcher
+        self.bot = TBot(BOT_TOKEN)
+        self.dispatcher = Dispatcher(self.bot, None, workers=0)
+        self.webhook_url = '/'.join((BOT_WEBHOOK_URL, BOT_WEBHOOK_TOKEN))
         self.register_handlers()
-        update_webhook_handler = StringCommandHandler(
-            'update_webhook',
-            self.update_webhook,
-        )
-        self.dispatcher.add_handler(update_webhook_handler)
 
     def register_handlers(self):
         pass
 
-    @staticmethod
-    def update_webhook(bot, update):
-        print('update_webhook')
-        
+    def update_webhook(self):
+        self.bot.set_webhook(
+            url=self.webhook_url
+        )
+
+    def process_request(self, url_path, update):
+
+        if url_path != '/' + BOT_WEBHOOK_TOKEN:
+            raise BotTokenError("Not for me.")
+
+        try:
+            update_json = loads(update)
+        except JSONDecodeError as e:
+            raise BotJSONError("Sent content isn't JSON.") from e
+
+        try:
+            self.dispatcher.process_update(
+                Update.de_json(update_json, self.bot)
+            )
+        except Exception as e:
+            raise BotLibraryError("Error in bot library.") from e
+
     def start_webhook(self, host='0.0.0.0', port=8080):
         self.updater.start_webhook(
             listen=host,
             port=port,
             url_path=BOT_WEBHOOK_TOKEN,
-            webhook_url='/'.join((BOT_WEBHOOK_URL, BOT_WEBHOOK_TOKEN))
+            webhook_url=self.webhook_url
         )
--- wsgi.py
+++ wsgi.py
@@ -1,21 +1,69 @@
-import logging
+from collections import namedtuple
+from logging import basicConfig, INFO
 
-from fjfnaranjobot.bot import Bot
+from fjfnaranjobot.bot import Bot, BotLibraryError, BotJSONError, BotTokenError
 
 
-logging.basicConfig(level=logging.INFO)
+basicConfig(level=INFO)
 
 bot = Bot()
-#bot.start_webhook()
+
+
+Response = namedtuple('Response', ['status', 'headers', 'data'])
 
 
 def application(environ, start_response):
-    """Simplest possible application object"""
-    data = 'Hello, World!\n'
-    status = '200 OK'
-    response_headers = [
-        ('Content-type','text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
-    start_response(status, response_headers)
-    return iter([data])
+    def prepare_text_response(text, status='200 OK', enconding='ascii'):
+        data = bytes(text, enconding)
+        status = status
+        response_headers = [
+            ('Content-type','text/plain'),
+            ('Content-Length', str(len(data)))
+        ]
+        return Response(status, response_headers, [data])
+
+    url_path = environ.get('RAW_URI', '/')
+    response = Response('404 Not Found', [], [])
+
+    # Root URL
+    if url_path == '/':
+        response = prepare_text_response("I'm fjfnaranjo's bot.")
+
+    # Healt check URL
+    elif url_path == '/ping':
+        response = prepare_text_response('pong')
+
+    # Register webhook request URL
+    elif url_path == '/register_webhook':
+        bot.update_webhook()
+        response = prepare_text_response('ok')
+
+    # Delegate response to bot library
+    else:
+
+        request_body_size = 0
+        try:
+            request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+        except ValueError:
+            pass
+
+        request_body = environ['wsgi.input'].read(request_body_size)
+
+        try:
+            bot.process_request(url_path, request_body)
+        except BotLibraryError as e:
+            raise e
+            response = prepare_text_response(
+                str(e),
+                status='500 Internal Server Error'
+            )
+        except BotJSONError as e:
+            response = prepare_text_response('', status='400 Bad Request')
+        except BotTokenError as e:
+            # Default value for response is 404 Not Found
+            pass
+        else:
+            response = Response('200 OK', [], [])
+
+    start_response(response.status, response.headers)
+    return iter(response.data)
