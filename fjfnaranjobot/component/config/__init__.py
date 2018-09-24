f599aa500f2f2c10d24a61ff72dc0c2e3245e07c
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2018-09-24 22:54:19 +0200

    Fix command names and command content unpacking first value

--- fjfnaranjobot/component/config/__init__.py
+++ fjfnaranjobot/component/config/__init__.py
@@ -15,24 +15,28 @@ cur = conn.cursor()
 
 
 def config_set_handler_processor(bot, update):
-    key, value = update.message.text.split(' ')
-    logger.info(f'Setting config {key} to {value}.')
+    _, key, value = update.message.text.split(' ')
+    logger.info(f"Setting config '{key}' to '{value}'.")
     cur.execute('INSERT INTO config VALUES (?, ?)', (key, value))
     
 
 def config_get_handler_processor(bot, update):
-    key = update.message.text
+    _, key = update.message.text
     cur.execute('SELECT value FROM config WHERE key=?', (key, ))
     result = cur.fetchone()
     value = result[0]
-    logger.info(f'Replying with value {value} for config {key}.')
-    update.message.reply_text(value)
+    if value is None:
+        logger.info(f"Replying with 'no value' message for config '{key}'.")
+        update.message.reply_text(f"No value for config '{key}'.")
+    else:
+        logger.info(f"Replying with '{value}' for config '{key}'.")
+        update.message.reply_text(value)
 
 
-config_set_handler = CommandHandler('data_set', config_set_handler_processor)
-config_get_handler = CommandHandler('data_get', config_get_handler_processor)
+config_set_handler = CommandHandler('config_set', config_set_handler_processor)
+config_get_handler = CommandHandler('config_get', config_get_handler_processor)
 
-logger.info('Initializing settings database.')
+logger.info("Initializing settings database.")
 cur.execute(
     'CREATE TABLE IF NOT EXISTS config (key PRIMARY KEY, value)'
 )
--- fjfnaranjobot/component/echo/__init__.py
+++ fjfnaranjobot/component/echo/__init__.py
@@ -8,7 +8,7 @@ logger = getLogger(__name__)
 
 def echo_handler_processor(bot, update):
     text = update.message.text
-    logger.info(f'Echoing "{text}" back.')
+    logger.info(f"Echoing '{text}' back.")
     bot.send_message(chat_id=update.message.chat_id, text=text)
 
 
