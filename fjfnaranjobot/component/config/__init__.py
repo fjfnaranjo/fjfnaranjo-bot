f3e69eede5e1235d093a1fbe0d889f3bc4415343
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2018-10-30 00:10:40 +0000

    Add threaded support to sqlite

--- fjfnaranjobot/component/config/__init__.py
+++ fjfnaranjobot/component/config/__init__.py
@@ -10,19 +10,21 @@ from fjfnaranjobot import BOT_DATA_DIR
 logger = getLogger(__name__)
 
 
-conn = connect(join(BOT_DATA_DIR, 'bot.db'))
-cur = conn.cursor()
 
 
 def config_set_handler_processor(bot, update):
     _, key, value = update.message.text.split(' ')
     logger.info(f"Setting config '{key}' to '{value}'.")
+    conn = connect(join(BOT_DATA_DIR, 'bot.db'))
+    cur = conn.cursor()
     cur.execute('INSERT INTO config VALUES (?, ?)', (key, value))
     conn.commit()
     
 
 def config_get_handler_processor(bot, update):
     _, key = update.message.text.split(' ')
+    conn = connect(join(BOT_DATA_DIR, 'bot.db'))
+    cur = conn.cursor()
     cur.execute('SELECT value FROM config WHERE key=?', (key, ))
     result = cur.fetchone()
     if result is None:
@@ -38,6 +40,10 @@ config_set_handler = CommandHandler('config_set', config_set_handler_processor)
 config_get_handler = CommandHandler('config_get', config_get_handler_processor)
 
 logger.info("Initializing settings database.")
-cur.execute(
+setup_conn = connect(join(BOT_DATA_DIR, 'bot.db'))
+setup_cur = setup_conn.cursor()
+setup_cur.execute(
     'CREATE TABLE IF NOT EXISTS config (key PRIMARY KEY, value)'
 )
+setup_cur.close()
+setup_conn.close()
