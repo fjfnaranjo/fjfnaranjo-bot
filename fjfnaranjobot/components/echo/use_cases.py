b3d54812ae4f3bc7e015cbdb120cd431c730252a
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-05-01 15:40:00 +0200

    Shorten echo log text

--- fjfnaranjobot/components/echo/use_cases.py
+++ fjfnaranjobot/components/echo/use_cases.py
@@ -6,5 +6,6 @@ logger = getLogger(__name__)
 
 def echo(bot, update):
     text = update.message.text
-    logger.info(f"Echoing '{text}' back.")
+    shown_text = text[:10]
+    logger.info(f"Echoing '{shown_text}' (cropped to 10 chars) back.")
     bot.send_message(chat_id=update.message.chat_id, text=text)
--- tests/components/echo/test_use_cases.py
+++ tests/components/echo/test_use_cases.py
@@ -4,10 +4,10 @@ from unittest.mock import patch, sentinel
 from fjfnaranjobot.components.echo.use_cases import echo
 
 
-@patch('telegram.bot.Bot')
 @patch('telegram.update.Update')
+@patch('telegram.bot.Bot')
 def test_echo_handler_processor(bot, update):
-    update.message.text = sentinel.text
+    update.message.text = 'msg'
     update.message.chat_id = sentinel.chat_id
     echo(bot, update)
-    bot.send_message.assert_called_with(chat_id=sentinel.chat_id, text=sentinel.text)
+    bot.send_message.assert_called_once_with(chat_id=sentinel.chat_id, text='msg')
