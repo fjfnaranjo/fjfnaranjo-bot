483f405ad1df133c93fa50e2fd65e5f145f55fc4
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-05-01 20:17:40 +0200

    Full cov for bot module views

--- tests/components/config/test_use_cases.py
+++ tests/components/config/test_use_cases.py
@@ -15,8 +15,21 @@ class ConfigTests(TestCase):
         update.message.reply_text.assert_called_once_with('ok')
 
     @patch('fjfnaranjobot.components.config.use_cases.get_key')
-    def test_config_get(self, get_key):
+    def test_config_get_missing(self, get_key):
         update = MagicMock()
         update.message.text = f'cmd key'
+        get_key.return_value = None
         config_get(update, None)
         get_key.assert_called_once_with('key')
+        update.message.reply_text.assert_called_once()
+        message_contents = update.message.reply_text.call_args[0][0]
+        assert 'No value' in message_contents
+
+    @patch('fjfnaranjobot.components.config.use_cases.get_key')
+    def test_config_get_exists(self, get_key):
+        update = MagicMock()
+        update.message.text = f'cmd key'
+        get_key.return_value = 'result'
+        config_get(update, None)
+        get_key.assert_called_once_with('key')
+        update.message.reply_text.assert_called_once_with('result')
