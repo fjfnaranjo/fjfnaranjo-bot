e6168071b97731d2cf0982baf460dad4c1856484
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-05-01 12:49:45 +0200

    Apply Black

--- fjfnaranjobot/logging.py
+++ fjfnaranjobot/logging.py
@@ -1,13 +1,19 @@
 from os import environ, access, W_OK
 
-from logging import getLogger as loggingGetLogger, INFO, DEBUG, WARNING, ERROR, CRITICAL 
+from logging import getLogger as loggingGetLogger, INFO, DEBUG, WARNING, ERROR, CRITICAL
 from logging.config import dictConfig
 
 
 BOT_LOGFILE = environ.get('BOT_LOGFILE')
 BOT_LOGLEVEL = environ.get('BOT_LOGLEVEL', 'INFO')
 
-valid_log_levels = {'DEBUG': DEBUG, 'INFO': INFO, 'WARNING': WARNING, 'ERROR': ERROR, 'CRITICAL': CRITICAL}
+valid_log_levels = {
+    'DEBUG': DEBUG,
+    'INFO': INFO,
+    'WARNING': WARNING,
+    'ERROR': ERROR,
+    'CRITICAL': CRITICAL,
+}
 
 
 class LoggingConfigured:
@@ -23,25 +29,29 @@ class LoggingConfigured:
     @classmethod
     def configure(cls):
         cls._configured = True
-        
+
 
 def configure_logging():
     if not access(BOT_LOGFILE, W_OK):
-        raise ValueError('Invalid file name in BOT_LOGFILE var.') 
+        raise ValueError('Invalid file name in BOT_LOGFILE var.')
     if BOT_LOGLEVEL not in valid_log_levels:
         raise ValueError('Invalid level in BOT_LOGLEVEL var.')
-    dictConfig({
-        'version': 1,
-        'formatters': {
-            'app': {'format': '{asctime} {levelname} [{name}] {msg}', 'style': '{'},
-        },
-        'handlers': {
-            'app': {'class': 'logging.FileHandler', 'formatter': 'app', 'filename': BOT_LOGFILE},
-        },
-        'loggers': {
-            'app': {'level': BOT_LOGLEVEL, 'handlers': ['app']}
-        },
-    })
+    dictConfig(
+        {
+            'version': 1,
+            'formatters': {
+                'app': {'format': '{asctime} {levelname} [{name}] {msg}', 'style': '{'},
+            },
+            'handlers': {
+                'app': {
+                    'class': 'logging.FileHandler',
+                    'formatter': 'app',
+                    'filename': BOT_LOGFILE,
+                },
+            },
+            'loggers': {'app': {'level': BOT_LOGLEVEL, 'handlers': ['app']}},
+        }
+    )
 
 
 def getLogger(name, level=None):
