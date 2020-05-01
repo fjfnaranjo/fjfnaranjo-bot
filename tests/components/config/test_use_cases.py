d1c1a9a8dc3650d225adab9de2f48c68bf988688
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-05-01 16:12:12 +0200

    Move def tests to TestCase

--- tests/components/config/test_use_cases.py
+++ tests/components/config/test_use_cases.py
@@ -1,21 +1,22 @@
+from unittest import TestCase
 from unittest.mock import patch, MagicMock
 
 
 from fjfnaranjobot.components.config.use_cases import config_set, config_get
 
 
-@patch('fjfnaranjobot.components.config.use_cases.set_key')
-def test_config_set(set_key):
-    update = MagicMock()
-    update.message.text = f'cmd key val'
-    config_set(update, None)
-    set_key.assert_called_once_with('key', 'val')
-    update.message.reply_text.assert_called_once_with('ok')
+class ConfigTests(TestCase):
+    @patch('fjfnaranjobot.components.config.use_cases.set_key')
+    def test_config_set(self, set_key):
+        update = MagicMock()
+        update.message.text = f'cmd key val'
+        config_set(update, None)
+        set_key.assert_called_once_with('key', 'val')
+        update.message.reply_text.assert_called_once_with('ok')
 
-
-@patch('fjfnaranjobot.components.config.use_cases.get_key')
-def test_config_get(get_key):
-    update = MagicMock()
-    update.message.text = f'cmd key'
-    config_get(update, None)
-    get_key.assert_called_once_with('key')
+    @patch('fjfnaranjobot.components.config.use_cases.get_key')
+    def test_config_get(self, get_key):
+        update = MagicMock()
+        update.message.text = f'cmd key'
+        config_get(update, None)
+        get_key.assert_called_once_with('key')
--- tests/components/echo/test_use_cases.py
+++ tests/components/echo/test_use_cases.py
@@ -1,11 +1,13 @@
+from unittest import TestCase
 from unittest.mock import MagicMock
 
 
 from fjfnaranjobot.components.echo.use_cases import echo
 
 
-def test_echo_handler_processor():
-    update = MagicMock()
-    update.message.text = 'msg'
-    echo(update, None)
-    update.message.reply_text.assert_called_once_with('msg')
+class EchoTests(TestCase):
+    def test_echo_handler_processor(self):
+        update = MagicMock()
+        update.message.text = 'msg'
+        echo(update, None)
+        update.message.reply_text.assert_called_once_with('msg')
