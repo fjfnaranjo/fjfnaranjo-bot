9ba8e30d9d238113bffe165a21a6ffe3f86cd26a
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-05-06 00:28:03 +0200

    Improve friends command text responses and logs

--- fjfnaranjobot/components/friends/use_cases.py
+++ fjfnaranjobot/components/friends/use_cases.py
@@ -14,27 +14,36 @@ logger = getLogger(__name__)
 
 
 def _friends_list(update):
-    logger.info(f"Sending list of friends.")
     all_friends = get_friends()
     if len(all_friends) == 0:
-        update.message.reply_text("You don't have any friend.")
+        logger.info(f"Sending no friends.")
+        update.message.reply_text("You don't have any friends.")
     else:
+        logger.info(f"Sending list of friends.")
         friend_list = ", ".join([str(id_) for id_ in all_friends])
         update.message.reply_text(f"Your friends are: {friend_list}")
 
 
 def _friend_add(update, id_):
-    logger.info(f"Adding @{id_} as a friend.")
-    ensure_int(id_)
-    add_friend(id_)
-    update.message.reply_text(f"Added ${id_} as a friend.")
+    id_int = ensure_int(id_)
+    if id_int not in get_friends():
+        logger.info(f"Adding @{id_int} as a friend.")
+        add_friend(id_int)
+        update.message.reply_text(f"Added @{id_int} as a friend.")
+    else:
+        logger.info(f"Not adding @{id_int} because already a friend.")
+        update.message.reply_text(f"@{id_int} is already a friend.")
 
 
 def _friend_del(update, id_):
-    logger.info(f"Removing @{id_} as a friend.")
-    ensure_int(id_)
-    del_friend(id_)
-    update.message.reply_text(f"Removed ${id_} as a friend.")
+    id_int = ensure_int(id_)
+    if id_int in get_friends():
+        logger.info(f"Removing @{id_int} as a friend.")
+        del_friend(id_int)
+        update.message.reply_text(f"Removed @{id_int} as a friend.")
+    else:
+        logger.info(f"Not removing @{id_int} because not a friend.")
+        update.message.reply_text(f"@{id_int} isn't a friend.")
 
 
 @only_owner
@@ -48,7 +57,9 @@ def friends(update, _context):
         elif command_parts[1] == 'del':
             _friend_del(update, command_parts[2])
         else:
-            update.message.reply_text(f"Uknown sub-command. Options are: add, del")
+            logger.info(f"Invalid syntax for /friends command. Unknown sub-command.")
+            update.message.reply_text(f"Unknown sub-command. Options are: add, del")
     else:
+        logger.info(f"Invalid syntax for /friends command. Usage sent.")
         update.message.reply_text(f"Invalid systax. Use: /friends [|add id|del id] .")
     raise DispatcherHandlerStop()
