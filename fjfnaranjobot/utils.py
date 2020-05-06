faa4a61487566d58e7440c2697b44d7d9eae2a32
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-05-06 18:03:04 +0200

    Add function to get bot data dir to utils

--- fjfnaranjobot/utils.py
+++ fjfnaranjobot/utils.py
@@ -1,2 +1,12 @@
+from os import environ
+
+
+_BOT_DATA_DIR_DEFAULT = ''
+
+
+def get_bot_data_dir():
+    return environ.get('BOT_DATA_DIR', _BOT_DATA_DIR_DEFAULT)
+
+
 class EnvValueError(Exception):
     pass
---
+++ tests/test_utils.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+from fjfnaranjobot.utils import get_bot_data_dir
+
+
+MODULE_PATH = 'fjfnaranjobot.utils'
+BOT_DATA_DIR_DEFAULT = ''
+BOT_DATA_DIR_TEST = '/bot/data/test'
+
+
+class UtilsTests(TestCase):
+    @patch.dict(f'{MODULE_PATH}.environ', {}, True)
+    def test_get_bot_data_dir_default(self):
+        assert get_bot_data_dir() == BOT_DATA_DIR_DEFAULT
+
+    @patch.dict(f'{MODULE_PATH}.environ', {'BOT_DATA_DIR': BOT_DATA_DIR_TEST})
+    def test_get_bot_data_dir_env(self):
+        assert get_bot_data_dir() == BOT_DATA_DIR_TEST
