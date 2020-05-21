7af52008559c6d804b2c5b71e6513f32e96dfd70
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-05-21 14:08:06 +0200

    Remove old aws module

--- fjfnaranjobot/aws.py
+++
@@ -1,5 +0,0 @@
-from os import environ
-
-AWS_DEFAULT_REGION = environ.get('AWS_DEFAULT_REGION')
-AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY')
--- tests/test_aws.py
+++
@@ -1,7 +0,0 @@
-from fjfnaranjobot.aws import AWS_ACCESS_KEY_ID  # noqa
-from fjfnaranjobot.aws import AWS_DEFAULT_REGION  # noqa
-from fjfnaranjobot.aws import AWS_SECRET_ACCESS_KEY  # noqa
-
-
-def test_dummy_aws():
-    pass
