28f630b1ac33630eba85e6e4f9bc1fbf2c743c86
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-05-25 16:26:10 +0200

    friends component handlers as config

--- fjfnaranjobot/components/friends/handlers.py
+++ fjfnaranjobot/components/friends/handlers.py
@@ -1,16 +1,80 @@
 from telegram.ext import CommandHandler, DispatcherHandlerStop
 
-from fjfnaranjobot.auth import only_owner
-from fjfnaranjobot.components.friends.use_cases import friends
+from fjfnaranjobot.auth import (
+    add_friend,
+    del_friend,
+    ensure_int,
+    get_friends,
+    only_owner,
+)
+from fjfnaranjobot.logging import getLogger
+
+logger = getLogger(__name__)
 
 group = 10
 
 
 @only_owner
-def friends_handler(update, _context):
-    command_parts = update.message.text.split(' ')
-    update.message.reply_text(friends(command_parts))
+def friends_get_handler(update, _context):
+    all_friends = get_friends()
+
+    if len(all_friends) == 0:
+        logger.info("Sending no friends.")
+        update.message.reply_text("You don't have any friends.")
+    elif len(all_friends) == 1:
+        logger.info("Sending one friend.")
+        the_friend = all_friends[0]
+        update.message.reply_text(f"You only have one friend: '{the_friend}'.")
+    elif len(all_friends) == 2:
+        logger.info("Sending two friends.")
+        first_friend, second_friend = all_friends
+        update.message.reply_text(
+            f"You have two friends: '{first_friend}' and '{second_friend}'."
+        )
+
+    else:
+        logger.info("Sending list of friends.")
+        last_friend = '\'' + str(all_friends[-1]) + '\''
+        other_friends = all_friends[:-1]
+        friend_list = ', '.join(['\'' + str(id_) + '\'' for id_ in other_friends])
+        update.message.reply_text(f"Your friends are: {friend_list} and {last_friend}.")
+
     raise DispatcherHandlerStop()
 
 
-handlers = (CommandHandler('friends', friends),)
+@only_owner
+def friends_add_handler(update, _context):
+    _, id_ = update.message.text.split(' ')
+    id_int = ensure_int(id_)
+
+    if id_int not in get_friends():
+        logger.info(f"Adding @{id_int} as a friend.")
+        add_friend(id_int)
+        update.message.reply_text(f"Added @{id_int} as a friend.")
+
+    else:
+        logger.info(f"Not adding @{id_int} because already a friend.")
+        update.message.reply_text(f"@{id_int} is already a friend.")
+
+    raise DispatcherHandlerStop()
+
+
+@only_owner
+def friends_del_handler(update, _context):
+    _, id_ = update.message.text.split(' ')
+    id_int = ensure_int(id_)
+
+    if id_int in get_friends():
+        logger.info(f"Removing @{id_int} as a friend.")
+        del_friend(id_int)
+        update.message.reply_text(f"Removed @{id_int} as a friend.")
+    else:
+        logger.info(f"Not removing @{id_int} because not a friend.")
+        update.message.reply_text(f"@{id_int} isn't a friend.")
+
+    raise DispatcherHandlerStop()
+
+
+handlers = (CommandHandler('friends_get', friends_get_handler),)
+handlers = (CommandHandler('friends_add', friends_add_handler),)
+handlers = (CommandHandler('friends_del', friends_del_handler),)
--- tests/components/friends/test_handlers.py
+++ tests/components/friends/test_handlers.py
@@ -1,19 +1,157 @@
 from unittest.mock import patch
 
-from telegram.ext import DispatcherHandlerStop
+from fjfnaranjobot.components.friends.handlers import (
+    friends_add_handler,
+    friends_del_handler,
+    friends_get_handler,
+    logger,
+)
 
-from fjfnaranjobot.components.friends.handlers import friends_handler
-
-from ...base import BotHandlerTestCase
+from ...base import (
+    FIRST_FRIEND_USERID,
+    SECOND_FRIEND_USERID,
+    THIRD_FRIEND_USERID,
+    BotHandlerTestCase,
+)
 
 MODULE_PATH = 'fjfnaranjobot.components.friends.handlers'
 
 
-@patch(f'{MODULE_PATH}.friends')
 class FriendsHandlersTests(BotHandlerTestCase):
-    def test_friends_called(self, friends):
+    def setUp(self):
+        BotHandlerTestCase.setUp(self)
         self._user_is_owner()
-        self._set_update_message_text('friends', ['arg1', 'arg2'])
-        with self.assertRaises(DispatcherHandlerStop):
-            friends_handler(self._update, None)
-        friends.assert_called_once_with(['friends', 'arg1', 'arg2'])
+
+    @patch(f'{MODULE_PATH}.get_friends', return_value=[])
+    def test_friends_get_no_friends(self, _get_friends):
+        self._set_update_message_text('friends_get', [])
+        with self._assert_reply_log_dispatch(
+            'You don\'t have any friends.', 'Sending no friends.', logger
+        ):
+            friends_get_handler(self._update, None)
+
+    @patch(f'{MODULE_PATH}.get_friends', return_value=[FIRST_FRIEND_USERID])
+    def test_friends_get_one_friend(self, _get_friends):
+        self._set_update_message_text('friends_get', [])
+        with self._assert_reply_log_dispatch(
+            f'You only have one friend: \'{FIRST_FRIEND_USERID}\'.',
+            'Sending one friend.',
+            logger,
+        ):
+            friends_get_handler(self._update, None)
+
+    @patch(
+        f'{MODULE_PATH}.get_friends',
+        return_value=[FIRST_FRIEND_USERID, SECOND_FRIEND_USERID],
+    )
+    def test_friends_get_two_friends(self, _get_friends):
+        self._set_update_message_text('friends_get', [])
+        with self._assert_reply_log_dispatch(
+            f'You have two friends: \'{FIRST_FRIEND_USERID}\' and \'{SECOND_FRIEND_USERID}\'.',
+            'Sending two friends.',
+            logger,
+        ):
+            friends_get_handler(self._update, None)
+
+    @patch(
+        f'{MODULE_PATH}.get_friends',
+        return_value=[FIRST_FRIEND_USERID, SECOND_FRIEND_USERID, THIRD_FRIEND_USERID],
+    )
+    def test_friends_get_many_friends(self, _get_friends):
+        self._set_update_message_text('friends_get', [])
+        with self._assert_reply_log_dispatch(
+            (
+                f'Your friends are: \'{FIRST_FRIEND_USERID}\', '
+                f'\'{SECOND_FRIEND_USERID}\' and \'{THIRD_FRIEND_USERID}\'.'
+            ),
+            'Sending list of friends.',
+            logger,
+        ):
+            friends_get_handler(self._update, None)
+
+    @patch(f'{MODULE_PATH}.get_friends', return_value=[])
+    @patch(f'{MODULE_PATH}.add_friend')
+    def test_friends_add_no_friends(self, add_friend, _get_friends):
+        self._set_update_message_text('friends_add', [f'{FIRST_FRIEND_USERID}'])
+        with self._assert_reply_log_dispatch(
+            f'Added @{FIRST_FRIEND_USERID} as a friend.',
+            f'Adding @{FIRST_FRIEND_USERID} as a friend.',
+            logger,
+        ):
+            friends_add_handler(self._update, None)
+        add_friend.assert_called_once_with(FIRST_FRIEND_USERID)
+
+    @patch(f'{MODULE_PATH}.get_friends', return_value=[FIRST_FRIEND_USERID])
+    @patch(f'{MODULE_PATH}.add_friend')
+    def test_friends_add_some_friends(self, add_friend, _get_friends):
+        self._set_update_message_text('friends_add', [f'{SECOND_FRIEND_USERID}'])
+        with self._assert_reply_log_dispatch(
+            f'Added @{SECOND_FRIEND_USERID} as a friend.',
+            f'Adding @{SECOND_FRIEND_USERID} as a friend.',
+            logger,
+        ):
+            friends_add_handler(self._update, None)
+        add_friend.assert_called_once_with(SECOND_FRIEND_USERID)
+
+    @patch(f'{MODULE_PATH}.get_friends', return_value=[FIRST_FRIEND_USERID])
+    @patch(f'{MODULE_PATH}.add_friend')
+    def test_friends_add_already_friend(self, add_friend, _get_friends):
+        self._set_update_message_text('friends_add', [f'{FIRST_FRIEND_USERID}'])
+        with self._assert_reply_log_dispatch(
+            f'@{FIRST_FRIEND_USERID} is already a friend.',
+            f'Not adding @{FIRST_FRIEND_USERID} because already a friend.',
+            logger,
+        ):
+            friends_add_handler(self._update, None)
+        add_friend.assert_not_called()
+
+    @patch(f'{MODULE_PATH}.get_friends', return_value=[])
+    @patch(f'{MODULE_PATH}.del_friend')
+    def test_friends_del_no_friends(self, del_friend, _get_friends):
+        self._set_update_message_text('friends_del', [f'{FIRST_FRIEND_USERID}'])
+        with self._assert_reply_log_dispatch(
+            f'@{FIRST_FRIEND_USERID} isn\'t a friend.',
+            f'Not removing @{FIRST_FRIEND_USERID} because not a friend.',
+            logger,
+        ):
+            friends_del_handler(self._update, None)
+        del_friend.assert_not_called()
+
+    @patch(f'{MODULE_PATH}.get_friends', return_value=[FIRST_FRIEND_USERID])
+    @patch(f'{MODULE_PATH}.del_friend')
+    def test_friends_del_not_friends(self, del_friend, _get_friends):
+        self._set_update_message_text('friends_del', [f'{SECOND_FRIEND_USERID}'])
+        with self._assert_reply_log_dispatch(
+            f'@{SECOND_FRIEND_USERID} isn\'t a friend.',
+            f'Not removing @{SECOND_FRIEND_USERID} because not a friend.',
+            logger,
+        ):
+            friends_del_handler(self._update, None)
+        del_friend.assert_not_called()
+
+    @patch(
+        f'{MODULE_PATH}.get_friends',
+        return_value=[FIRST_FRIEND_USERID, SECOND_FRIEND_USERID],
+    )
+    @patch(f'{MODULE_PATH}.del_friend')
+    def test_friends_del_some_friends(self, del_friend, _get_friends):
+        self._set_update_message_text('friends_del', [f'{SECOND_FRIEND_USERID}'])
+        with self._assert_reply_log_dispatch(
+            f'Removed @{SECOND_FRIEND_USERID} as a friend.',
+            f'Removing @{SECOND_FRIEND_USERID} as a friend.',
+            logger,
+        ):
+            friends_del_handler(self._update, None)
+        del_friend.assert_called_once_with(SECOND_FRIEND_USERID)
+
+    @patch(f'{MODULE_PATH}.get_friends', return_value=[FIRST_FRIEND_USERID])
+    @patch(f'{MODULE_PATH}.del_friend')
+    def test_friends_del_last_friend(self, del_friend, _get_friends):
+        self._set_update_message_text('friends_del', [f'{FIRST_FRIEND_USERID}'])
+        with self._assert_reply_log_dispatch(
+            f'Removed @{FIRST_FRIEND_USERID} as a friend.',
+            f'Removing @{FIRST_FRIEND_USERID} as a friend.',
+            logger,
+        ):
+            friends_del_handler(self._update, None)
+        del_friend.assert_called_once_with(FIRST_FRIEND_USERID)
