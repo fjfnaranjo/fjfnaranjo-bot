c65429a7293313b60ecd55df207acb6fe7356b46
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-06-01 14:52:48 +0200

    Separate terraria and terraria_admin commands

--- fjfnaranjobot/components/terraria/handlers.py
+++ fjfnaranjobot/components/terraria/handlers.py
@@ -1,17 +1,29 @@
 from telegram.ext import CommandHandler, DispatcherHandlerStop
 
-from fjfnaranjobot.auth import only_owner
+from fjfnaranjobot.auth import only_owner, only_real
 from fjfnaranjobot.logging import getLogger
 
 logger = getLogger(__name__)
 
 
-@only_owner
+@only_real
 def terraria_handler(_update, _context):
     logger.info("The terraria command is not implemented.")
     raise DispatcherHandlerStop()
 
 
-handlers = (CommandHandler('terraria', terraria_handler),)
+@only_owner
+def terraria_admin_handler(_update, _context):
+    logger.info("The terraria admin command is not implemented.")
+    raise DispatcherHandlerStop()
 
-commands = (("Manage Terraria servers.", 'terraria', 'terraria'),)
+
+handlers = (
+    CommandHandler('terraria', terraria_handler),
+    CommandHandler('terraria_admin', terraria_admin_handler),
+)
+
+commands = (
+    ("Manage Terraria servers.", 'terraria', 'terraria'),
+    ("Manage Terraria servers (admin).", None, 'terraria_admin'),
+)
--- tests/components/terraria/test_handlers.py
+++ tests/components/terraria/test_handlers.py
@@ -1,4 +1,8 @@
-from fjfnaranjobot.components.terraria.handlers import logger, terraria_handler
+from fjfnaranjobot.components.terraria.handlers import (
+    logger,
+    terraria_admin_handler,
+    terraria_handler,
+)
 
 from ...base import BotHandlerTestCase
 
@@ -9,6 +13,12 @@ class TerrariaHandlersTests(BotHandlerTestCase):
     def setUp(self, *args, **kwargs):
         BotHandlerTestCase.setUp(self, *args, **kwargs)
         self.user_is_owner()
+
+    def test_terraria_admin_handler_does_nothing(self):
+        with self.assert_log_dispatch(
+            'The terraria admin command is not implemented.', logger
+        ):
+            terraria_admin_handler(*self.update_and_context)
 
     def test_terraria_handler_does_nothing(self):
         with self.assert_log_dispatch(
