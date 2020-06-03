ad375bfc7c1ed1949308637689d3400d7e0ecd99
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-06-03 15:49:55 +0200

    Add beat scheduler configuration discovery

--- fjfnaranjobot/tasks.py
+++ fjfnaranjobot/tasks.py
@@ -1,3 +1,5 @@
+from importlib import import_module
+
 from celery import Celery
 
 from fjfnaranjobot.common import get_bot_components
@@ -6,13 +8,46 @@ from fjfnaranjobot.logging import getLogger
 logger = getLogger(__name__)
 
 
-_TASKS_COMPONENTS_TEMPLATE = 'fjfnaranjobot.components.{}'
+_COMPONENTS_TEMPLATE = 'fjfnaranjobot.components.{}'
+_TASKS_COMPONENTS_TEMPLATE = 'fjfnaranjobot.components.{}.tasks'
 
 
 app = Celery('tasks')
-app.autodiscover_tasks(
-    [
-        _TASKS_COMPONENTS_TEMPLATE.format(component)
-        for component in get_bot_components().split(',')
-    ],
-)
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **_kwargs):
+    for component in get_bot_components().split(','):
+        try:
+            component_tasks = import_module(
+                _TASKS_COMPONENTS_TEMPLATE.format(component)
+            )
+
+        except ModuleNotFoundError:
+            pass
+        else:
+            try:
+                schedule = component_tasks.schedule
+            except AttributeError:
+                pass
+            else:
+                for entry in schedule:
+                    schedule_def = schedule[entry].copy()
+                    del schedule_def['schedule']
+                    del schedule_def['signature']
+                    sender.add_periodic_task(
+                        schedule[entry]['schedule'],
+                        schedule[entry]['signature'],
+                        name=entry,
+                        **schedule_def
+                    )
+
+
+@app.on_after_finalize.connect
+def setup_tasks(sender, **_kwargs):
+    sender.autodiscover_tasks(
+        [
+            _COMPONENTS_TEMPLATE.format(component)
+            for component in get_bot_components().split(',')
+        ],
+    )
