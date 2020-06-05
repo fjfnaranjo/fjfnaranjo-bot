7ce4d5bb88534b0c9f7fff0e31333520d956bc94
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-06-05 15:17:58 +0200

    Simple test for terraria profiles model

---
+++ tests/components/terraria/test_models.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+from fjfnaranjobot.components.terraria.models import TerrariaProfile
+
+
+class TerrariaModelsTests(TestCase):
+    def test_model(self):
+        TerrariaProfile()
