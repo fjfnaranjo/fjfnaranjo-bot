2d2a7e5cc9f04ca0c4ca84814418e5b341e20bd7
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-06-17 18:49:40 +0200

    Simple players read for activity log (happy case only)

--- fjfnaranjobot/components/terraria/tasks.py
+++ fjfnaranjobot/components/terraria/tasks.py
@@ -1,58 +1,81 @@
+from boto3 import resource
 from celery import chain, task
+from requests import get as requests_get
 
+from fjfnaranjobot.components.terraria.models import TerrariaProfile
 from fjfnaranjobot.logging import getLogger
 
 logger = getLogger(__name__)
 
 
+_MICROAPI_URL = 'http://{}:7979/{}/'
+_TSHOCK_URL = 'http://{}:7878/{}?token={}'
+
+
 @task(bind=True)
-def _giving_ec2_instance_state(self, message_id, profile_id):
+def _giving_ec2_instance_state_and_ip(self, profile_id, message_id):
     logger.debug(f"Entering inner task {self.name} .")
-    instance_state = None
-    return instance_state, message_id, profile_id
+    profile = TerrariaProfile(profile_id)
+    ec2 = resource(
+        'ec2',
+        region_name=profile.aws_default_region,
+        aws_access_key_id=profile.aws_access_key_id,
+        aws_secret_access_key=profile.aws_secret_access_key,
+    )
+    instance = ec2.Instance(profile.instance_id)
+    return instance.public_ip_address, instance.state['Name'], message_id, profile_id
 
 
 @task(bind=True)
 def _report_on_faulty_ec2_instance_state(self, last_args):
     logger.debug(f"Entering inner task {self.name} .")
-    _instance_state, message_id, profile_id = last_args
-    return message_id, profile_id
+    instance_ip, _instance_state, message_id, profile_id = last_args
+    return instance_ip, message_id, profile_id
 
 
 @task(bind=True)
 def _giving_microapi_state(self, last_args):
     logger.debug(f"Entering inner task {self.name} .")
-    message_id, profile_id = last_args
-    microapi_state = None
-    return microapi_state, message_id, profile_id
+    instance_ip, message_id, profile_id = last_args
+    profile = TerrariaProfile(profile_id)
+    status_url = _MICROAPI_URL.format(instance_ip, profile.microapi_token) + 'status'
+    microapi_state = requests_get(status_url).text
+    return microapi_state, instance_ip, message_id, profile_id
 
 
 @task(bind=True)
 def _report_on_faulty_microapi_state(self, last_args):
     logger.debug(f"Entering inner task {self.name} .")
-    _microapi_state, message_id, profile_id = last_args
-    return message_id, profile_id
+    _microapi_state, instance_ip, message_id, profile_id = last_args
+    return instance_ip, message_id, profile_id
 
 
 @task(bind=True)
-def _giving_tshock_state(self, last_args):
+def _giving_tshock_players(self, last_args):
     logger.debug(f"Entering inner task {self.name} .")
-    message_id, profile_id = last_args
-    tshock_state = None
-    return tshock_state, message_id, profile_id
+    instance_ip, message_id, profile_id = last_args
+    profile = TerrariaProfile(profile_id)
+    active_list_url = _TSHOCK_URL.format(
+        instance_ip, 'v2/server/status', profile.tshock_token
+    )
+    active_list_url = active_list_url + '&players=true'
+    tshock_status = requests_get(active_list_url).json()
+    players = tshock_status['players']
+    return players, message_id, profile_id
 
 
 @task(bind=True)
 def _report_on_faulty_tshock_state(self, last_args):
     logger.debug(f"Entering inner task {self.name} .")
-    _tshock_state, message_id, profile_id = last_args
-    return message_id, profile_id
+    players, message_id, profile_id = last_args
+    return players, message_id, profile_id
 
 
 @task(bind=True)
 def _log_user_activity(self, last_args):
     logger.debug(f"Entering inner task {self.name} .")
-    _message_id, _profile_id = last_args
+    players, _message_id, _profile_id = last_args
+    logger.error(str(players))
 
 
 def log_user_activity_chain(profile_id, message_id):
@@ -61,11 +84,11 @@ def log_user_activity_chain(profile_id, message_id):
         f"profile id {profile_id} and message_id {message_id}."
     )
     return chain(
-        _giving_ec2_instance_state.s(message_id, profile_id),
+        _giving_ec2_instance_state_and_ip.s(profile_id, message_id),
         _report_on_faulty_ec2_instance_state.s(),
         _giving_microapi_state.s(),
         _report_on_faulty_microapi_state.s(),
-        _giving_tshock_state.s(),
+        _giving_tshock_players.s(),
         _report_on_faulty_tshock_state.s(),
         _log_user_activity.s(),
     )()
--- fjfnaranjobot/components/terraria/utils.py
+++ fjfnaranjobot/components/terraria/utils.py
@@ -1,18 +1,4 @@
-from boto3 import resource
-
-from fjfnaranjobot.components.terraria.models import TerrariaProfile
-
 _SECONDS_PER_PORTION = 60 * 5
-
-
-def _resource_from_profile_id(service_name, profile_id):
-    profile = TerrariaProfile(profile_id)
-    return resource(
-        service_name,
-        region_name=profile.aws_default_region,
-        aws_access_key_id=profile.aws_access_key_id,
-        aws_secret_access_key=profile.aws_secret_access_key,
-    )
 
 
 def aproximate_datetime(datetime):
