69347a708b5eb6768ddf04eb5daef9b288332f06
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-11-02 13:42:21 +0100

    Log to stdout

--- fjfnaranjobot/logging.py
+++ fjfnaranjobot/logging.py
@@ -4,8 +4,9 @@ from logging.config import dictConfig
 from os import environ, makedirs, remove
 from os.path import isdir, isfile, join, split
 
-_BOT_LOGFILE_DEFAULT = 'botdata/bot.log'
 _BOT_LOGLEVEL_DEFAULT = 'INFO'
+
+_configured = False
 
 valid_log_levels = {
     'DEBUG': DEBUG,
@@ -16,30 +17,8 @@ valid_log_levels = {
 }
 
 
-_state = {'initialized': False}
-
-
-def _get_log_path():
-    return environ.get('BOT_LOGFILE', _BOT_LOGFILE_DEFAULT)
-
-
 def _configure_logging():
-    log_path = _get_log_path()
     log_level = environ.get('BOT_LOGLEVEL', _BOT_LOGLEVEL_DEFAULT)
-    log_dir, _ = split(log_path)
-    if not isdir(log_dir):
-        try:
-            makedirs(log_dir)
-        except:
-            raise ValueError("Invalid dir name in BOT_LOGFILE var.")
-    new_file = False
-    if not isfile(log_path):
-        try:
-            with open(log_path, 'wb'):
-                pass
-            new_file = True
-        except OSError:
-            raise ValueError("Invalid file name in BOT_LOGFILE var.")
     if log_level not in valid_log_levels:
         raise ValueError("Invalid level in BOT_LOGLEVEL var.")
     dictConfig(
@@ -53,29 +32,25 @@ def _configure_logging():
             },
             'handlers': {
                 'app': {
-                    'class': 'logging.FileHandler',
+                    'class': 'logging.StreamHandler',
                     'formatter': 'app',
-                    'filename': log_path,
                 },
             },
             'loggers': {'app': {'level': log_level, 'handlers': ['app']}},
         }
     )
-    if new_file:
-        loggingGetLogger('app').info("Log created.")
-    _state['initialized'] = True
 
 
-def reset():
-    _state['initialized'] = False
-    log_path = _get_log_path()
-    if isfile(log_path):
-        remove(log_path)
+def reset_logging():
+    global _configured
+    if not _configured:
+        return
     _configure_logging()
 
 
 def getLogger(name, level=None):
-    if not _state['initialized']:
+    global _configured
+    if not _configured:
         _configure_logging()
     logger = loggingGetLogger(f'app.{name}')
     if level is not None:
--- tests/test_logging.py
+++ tests/test_logging.py
@@ -3,83 +3,25 @@ from os.path import isfile, join
 from tempfile import mkdtemp, mkstemp
 from unittest.mock import patch
 
-from fjfnaranjobot.logging import _get_log_path, getLogger, reset
+from fjfnaranjobot.logging import getLogger, reset_logging
 
 from .base import BotTestCase
 
 MODULE_PATH = 'fjfnaranjobot.logging'
 
-BOT_LOGFILE_DEFAULT = 'botdata/bot.log'
-BOT_LOGFILE_TEST = 'test/bot.a.test.name.log'
-LOGFILE_TEST_FILE = mkstemp()[1]
-LOGFILE_TEST_DIR = mkdtemp()
 FAKE_LEVEL = 99
 
 
 class LoggingTests(BotTestCase):
-    def test_get_log_path_default(self):
-        with self.mocked_environ(
-            f'{MODULE_PATH}.environ',
-            None,
-            [
-                'BOT_LOGFILE',
-            ],
-        ):
-            assert _get_log_path() == BOT_LOGFILE_DEFAULT
-
-    def test_get_log_path_join_and_env(self):
-        with self.mocked_environ(
-            f'{MODULE_PATH}.environ',
-            {'BOT_LOGFILE': BOT_LOGFILE_TEST},
-        ):
-            assert _get_log_path() == BOT_LOGFILE_TEST
-
-    @patch(f'{MODULE_PATH}._get_log_path', return_value=LOGFILE_TEST_FILE)
-    def test_reset_creates_new(self, _get_log_path):
-        with open(LOGFILE_TEST_FILE, 'wb') as temp_file:
-            temp_file.write(b'notempty')
-        reset()
-        with open(LOGFILE_TEST_FILE, 'rb') as temp_file:
-            log_content = temp_file.read()
-            assert b'notempty' != log_content
-            assert 'Log created.' in log_content.decode('utf8')
-
-    @patch(f'{MODULE_PATH}._get_log_path', return_value=join(LOGFILE_TEST_FILE, 'dir'))
-    def test_reset_invalid_log_dir_name(self, _get_log_path):
-        with self.assertRaises(ValueError) as e:
-            reset()
-        assert 'Invalid dir name in BOT_LOGFILE var.' == e.exception.args[0]
-
-    @patch(
-        f'{MODULE_PATH}._get_log_path',
-        return_value=join(LOGFILE_TEST_DIR, 'file'),
-    )
-    def test_reset_invalid_db_file_name(self, _get_log_path):
-        chmod(LOGFILE_TEST_DIR, 0)
-        with self.assertRaises(ValueError) as e:
-            reset()
-        assert 'Invalid file name in BOT_LOGFILE var.' == e.exception.args[0]
-
-    @patch(f'{MODULE_PATH}._get_log_path', return_value=LOGFILE_TEST_FILE)
-    def test_logger_no_valid_log_level(self, _get_log_path):
+    def test_logger_no_valid_log_level(self):
         with self.mocked_environ(
             f'{MODULE_PATH}.environ', {'BOT_LOGLEVEL': f'{FAKE_LEVEL}'}
         ):
             with self.assertRaises(ValueError) as e:
-                reset()
+                reset_logging()
+                getLogger('fbtest')
             assert 'Invalid level in BOT_LOGLEVEL var.' == e.exception.args[0]
 
-    @patch(f'{MODULE_PATH}._get_log_path', return_value=LOGFILE_TEST_FILE)
-    def test_logger_no_default_name(self, _get_log_path):
-        with self.assertRaises(TypeError):
-            getLogger()
-
-    @patch(f'{MODULE_PATH}._get_log_path', return_value=LOGFILE_TEST_FILE)
-    def test_logger_init_log(self, _get_log_path):
-        reset()
-        assert isfile(LOGFILE_TEST_FILE)
-
-    @patch(f'{MODULE_PATH}._get_log_path', return_value=LOGFILE_TEST_FILE)
-    def test_logger_alt_level(self, _get_log_path):
+    def test_logger_alt_level(self):
         logger = getLogger('fbtest', FAKE_LEVEL)
         assert logger.getEffectiveLevel() == FAKE_LEVEL
