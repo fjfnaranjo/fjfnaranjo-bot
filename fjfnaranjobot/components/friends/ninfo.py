987b9abf473cde66e675c9b8e8102e159fd5b3a5
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-11-08 13:53:18 +0100

    Remove Friends command from production command list

--- fjfnaranjobot/components/friends/ninfo.py
+++ fjfnaranjobot/components/friends/ninfo.py
@@ -1,7 +1,9 @@
-from fjfnaranjobot.command import BotCommand
+from fjfnaranjobot.command import BotCommand, CommandHandlerMixin
 
 
-class Friends(BotCommand):
+class Friends(BotCommand, CommandHandlerMixin):
     command_name = "nfriends"
     description = "Manage friends."
-    is_prod_command = True
+
+    def handle_command(self):
+        self.reply("Not implemented.")
