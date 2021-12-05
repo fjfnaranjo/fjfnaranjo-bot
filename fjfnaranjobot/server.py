579d0034f88c38b81f27c368dc0077e0d7de4b56
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2021-12-05 21:10:00 +0100

    black isort

--- fjfnaranjobot/server.py
+++ fjfnaranjobot/server.py
@@ -1,11 +1,11 @@
 def main():
     import bjoern
- 
+
     from fjfnaranjobot.wsgi import application
- 
 
     bjoern.listen(application, "0.0.0.0", 8001)
     bjoern.run()
+
 
 if __name__ == "__main__":
     main()
