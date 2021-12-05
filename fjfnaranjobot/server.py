c71c88c63db90464843ab5cd3e1dfc67e535577a
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2021-12-05 20:56:42 +0100

    Add __main__ guard to server.py

--- fjfnaranjobot/server.py
+++ fjfnaranjobot/server.py
@@ -1,6 +1,11 @@
-import bjoern
+def main():
+    import bjoern
+ 
+    from fjfnaranjobot.wsgi import application
+ 
 
-from fjfnaranjobot.wsgi import application
+    bjoern.listen(application, "0.0.0.0", 8001)
+    bjoern.run()
 
-bjoern.listen(application, "0.0.0.0", 8001)
-bjoern.run()
+if __name__ == "__main__":
+    main()
