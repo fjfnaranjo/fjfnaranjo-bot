21645e1901f3b25d7f268381ced477ca126d4369
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2026-03-05 11:33:10 +0100

    Implement terraria command (new style)

---
+++ fjfnaranjobot/components/terraria/handlers/nterraria.py
@@ -0,0 +1,34 @@
+from fjfnaranjobot.command import BotCommand, CommandHandlerMixin
+from fjfnaranjobot.logging import getLogger
+
+logger = getLogger(__name__)
+
+#
+# terraria_handler states
+#
+# transition key: -o-> only owner
+#                 -p-> only users in profile
+#                 -f-> only friends
+#
+# cmd ---> SELECT_ACTION -o-> CHAT_REGISTER_CHAT_PROFILE ---> end
+#                        -f-> REQUEST_SINGUP ---> end
+#                        -r-> SHOW_STATUS ---> end
+#                        -r-> BROADCAST_STATUS ---> end
+#                        -r-> SHOW_USAGE_REPORT ---> end
+#                        -r-> BROADCAST_USAGE_REPORT ---> end
+#                        -r-> CHAT_START ---> CHAT_START_CONFIRM ---> end
+#                        -r-> CHAT_STOP ---> CHAT_STOP_CONFIRM ---> end
+#                        -r-> CHAT_HOLD ---> CHAT_HOLD_CONFIRM --> end
+#
+
+
+class Terraria(CommandHandlerMixin, BotCommand):
+    permissions = BotCommand.PermissionsEnum.ONLY_REAL
+    command_name = "nterraria"
+    description = "Manage Terraria servers."
+    allow_chats = True
+
+    async def entrypoint(self):
+        reply = "The 'terraria' command is not implemented."
+        logger.warning(reply)
+        await self.reply(reply)
---
+++ fjfnaranjobot/components/terraria/ninfo.py
@@ -0,0 +1,5 @@
+from fjfnaranjobot.components.terraria.handlers.nterraria import Terraria as _Terraria
+
+
+class Terraria(_Terraria):
+    pass
